98db996c86a4920751154619b78ddd75bb420d99
valentino1996 <valentino.isufi1@gmail.com>
2016-11-08 18:49:09 +0100

    fixing

--- public/main.js
+++ public/main.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
 				author: $("#authorInput").val()
 			},
 			success: function(){
-				("p").html("book stored!");
+				$("p").html("book stored!");
 			}
 			
 		});
